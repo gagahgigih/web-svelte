25029476ddb9db64b6fca33ec6ea76eb9c93d766
dwissaaj <dwisaji150@gmail.com>
2023-09-11 19:02:25 +0700

    add faq,gallery, and acc page

---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import {Accordion, AccordionItem} from "@skeletonlabs/skeleton"
+</script>
+<svelte:head>
+  <title>FAQ | Gagah Gigih</title>
+  <meta name="description" content="Self Studio Photo Terbaik di Pangkalan Bun" />
+  <meta name="keywords" content="Foto, Studio Foto, Pangkalan Bun">
+  <meta name="Gagah Gigih" content="Gagah Gigih Studio">
+</svelte:head>
+<section>
+    <div class="w-full h-screen bg-cover bg-faq-pattern bg-no-repeat bg-top bg-fixed p-8">
+        <div class="mx-auto container">
+            <div>
+                <h1 class="h1 text-bold my-3">Yang sering ditanyakan</h1>
+            </div>
+            <div class="flex justify-center items-center">
+                <Accordion class="varian-filled-primary bg-pink-500/70 rounded-md p-2 text-white">
+                    <AccordionItem   open>
+                        <svelte:fragment  slot="summary">
+                            <p class="bg-secondary-500 p-2 font-bold text-lg rounded-md">Lokasi dimana kak?</p>
+                        </svelte:fragment>
+                        <svelte:fragment slot="content"><p class="bg-pink-500/50 underline text-lg font-semibold p-2 rounded-md">Di Kumai ya</p>
+                    </svelte:fragment>
+                    </AccordionItem>
+                    <AccordionItem   open>
+                        <svelte:fragment  slot="summary">
+                            <p class="bg-secondary-500 p-2 font-bold text-lg rounded-md">Bagaimana dengan Pakaian?</p>
+                        </svelte:fragment>
+                        <svelte:fragment slot="content"><p class="bg-pink-500/50 underline text-lg font-semibold p-2 rounded-md">Fashion terserah kalian</p>
+                    </svelte:fragment>
+                    </AccordionItem>
+                    <AccordionItem   open>
+                        <svelte:fragment  slot="summary">
+                            <p class="bg-secondary-500 p-2 font-bold text-lg rounded-md">Apakah ada aturan khusus?</p>
+                        </svelte:fragment>
+                        <svelte:fragment slot="content"><p class="bg-pink-500/50 underline text-lg font-semibold p-2 rounded-md">Sepatu dilarang dipakai di studio</p>
+                    </svelte:fragment>
+                    </AccordionItem>
+                    <!-- ... -->
+                </Accordion>
+            </div>
+        </div>
+    </div>   
+</section>
--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,1 +1,43 @@
-<h1>klj</h1>
+<script>
+    import img1 from '$lib/images/1.jpg';
+    import img2 from '$lib/images/2.jpg';
+    import img3 from '$lib/images/3.jpg';
+    import img4 from '$lib/images/4.jpg';
+    import img5 from '$lib/images/5.jpg';
+    import img6 from '$lib/images/6.jpg';
+</script>
+<svelte:head>
+  <title>Gallery | Gagah Gigih</title>
+  <meta name="description" content="Gallery kami photo dari Self Studio Photo Terbaik di Pangkalan Bun" />
+  <meta name="keywords" content="Foto, Studio Foto, Pangkalan Bun, Gallery">
+  <meta name="Gagah Gigih" content="Gagah Gigih Studio">
+</svelte:head>
+<section>
+    <div class="w-full bg-cover bg-gallery-pattern bg-no-repeat bg-top bg-fixed p-8">
+        <div class="flex flex-col justify-center items-center">
+            <div>
+                <h1 class="h1 text-bold my-6"> Gallery Kami</h1>
+            </div>
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+                <div class="p-4 hover:scale-110 transition duration-300 ease-in-out">
+                    <img class="border border-primary-500 rounded-md shadow-md  shadow-pink-500"  src={img1} alt="" />
+                </div>
+                <div class="p-4 hover:scale-110 transition duration-300 ease-in-out">
+                    <img class="border border-primary-500 rounded-md shadow-md  shadow-pink-500" src={img2} alt="" />
+                </div>
+                <div class="p-4 hover:scale-110 transition duration-300 ease-in-out">
+                    <img class="border border-primary-500 rounded-md shadow-md  shadow-pink-500" src={img3} alt="" />
+                </div>
+                <div class="p-4 hover:scale-110 transition duration-300 ease-in-out">
+                    <img class="border border-primary-500 rounded-md shadow-md  shadow-pink-500" src={img4} alt="" />
+                </div>
+                <div class="p-4 hover:scale-110 transition duration-300 ease-in-out">
+                    <img class="border border-primary-500 rounded-md shadow-md  shadow-pink-500" src={img5} alt="" />
+                </div>
+                <div class="p-4 hover:scale-110 transition duration-300 ease-in-out">
+                    <img class="border border-primary-500 rounded-md shadow-md  shadow-pink-500" src={img6} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>        
