87ff60c2e8e96a9353d1bb1d8c2732bac3ffbc0f
dwissaaj <dwisaji150@gmail.com>
2023-09-12 20:10:20 +0700

    add missing link

--- src/lib/introduction.svelte
+++ src/lib/introduction.svelte
@@ -12,15 +12,21 @@
             Siapa?
         </p></Tab>
         <Tab bind:group={tabSet} name="tab3" value={2}> <p class="text-xl">Dimana?</p></Tab>
-    
+        
+        <Tab bind:group={tabSet} name="tab4" value={3}> <p class="text-xl">Bagaimana?</p></Tab>
+        <Tab bind:group={tabSet} name="tab5" value={4}> <p class="text-xl">Kapan?</p></Tab>
         <!-- Tab Panels --->
         <svelte:fragment slot="panel">
             {#if tabSet === 0}
-               <p class="flex justify-center items-center">Gagah Gigih adalah self studio photo di Pangkalan Bun</p>
+               <p class="flex justify-center items-center">Gagah Gigih adalah Self Studio Photo di Pangkalan Bun yang menggabungkan seni dan teknologi</p>
             {:else if tabSet === 1}
-               <p class="flex justify-center items-center">Kami berpengalaman di bidang industri kreatif</p>
+                <p class="flex flex-col justify-center items-center">Anggota kami adalah content creator, designer, photographer & videographer, data analyst, bahkan IT Developer </p>
             {:else if tabSet === 2}
-            <p class="flex justify-center items-center">Lokasi Studio Kami di Kumai</p>
+            <p class="flex justify-center items-center">Lokasi Studio Kami Jln Bendaha Kumai BTN Kumai Damai GG. Pepaya No. 31</p>
+            {:else if tabSet === 3}
+            <p class="flex justify-center items-center">Studio Kami di design para cinematographer dan photographer untuk menghasilkan foto yang indah</p>
+            {:else if tabSet === 4}
+            <a href="https://www.youtube.com/MultimediaPIctures" > Studio ini berdiri pada tahun 2023 tetapi kiprah kami di industri kreatif sudah sejak 2013 dimulai dengan Multimedia Pictures</a>
             {/if}
         </svelte:fragment>
     </TabGroup>
